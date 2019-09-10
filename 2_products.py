25161d835c169311a60cf3a4d006fa4f02754c05
Juan Ignacio Yegro <nachoyegro@gmail.com>
2019-09-10 13:33:12 -0300

    #2 Products: proposed solution

--- 2_products.py
+++ 2_products.py
@@ -14,7 +14,10 @@
         The result list contains on every position, the total divided by the element at the actual position
 
     Solution 2: Without using division
-        I still didn't come up with a solution for this
+        The proposed solution is having two lists:
+            - The first list has on each position the product of all prefixes
+            - The second list has on each position the product of all sufixes
+            - The result list
 """
 
 def multiply(elements):
@@ -32,8 +35,39 @@ def solution_with_division(elements):
         result.append(total / elem)
     return result
 
-def solution_without_division(elements):
-    pass
+def solution_without_division(nums):
+    # Generate prefix products
+    prefix_products = []
+    # Iterate over list of nums: O(N)
+    for num in nums:
+        if prefix_products:
+            prefix_products.append(prefix_products[-1] * num)
+        else:
+            prefix_products.append(num)
+
+    # Generate suffix products
+    suffix_products = []
+    # Iterate over list of nums: O(N)
+    for num in reversed(nums):
+        if suffix_products:
+            suffix_products.append(suffix_products[-1] * num)
+        else:
+            suffix_products.append(num)
+    suffix_products = list(reversed(suffix_products))
+
+    # Generate result
+    result = []
+    # Iterate over a range of len(nums): O(N)
+    for i in range(len(nums)):
+        if i == 0:
+            result.append(suffix_products[i + 1])
+        elif i == len(nums) - 1:
+            result.append(prefix_products[i - 1])
+        else:
+            result.append(prefix_products[i - 1] * suffix_products[i + 1])
+    return result
+
 
 if __name__ == '__main__':
     print solution_with_division([1,2,3,4,5])
+    print solution_without_division([1,2,3,4,5])
